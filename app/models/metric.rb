ed5ab919a26943c600a4f0a56ef768d24be5ace6
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-09 23:31:17 +0300

    Changed validations in Metric model with FIXME comment

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,3 +1,6 @@
 class Metric < ApplicationRecord
   belongs_to :driver
+
+  # FIXME: Remove next line if business would like to save garbage metrics
+  validates :metric_name, :value, presence: true
 end
