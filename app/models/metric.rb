c9eea1202ce42572fd8bf8d02f9f81ad388b1d84
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-09 23:33:40 +0300

    Maked driver relation is optional in Metric model

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,5 +1,5 @@
 class Metric < ApplicationRecord
-  belongs_to :driver
+  belongs_to :driver, optional: true
 
   # FIXME: Remove next line if business would like to save garbage metrics
   validates :metric_name, :value, presence: true
