9a687e6331b1e93340da7ba6cddc912f2d328261
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-10 00:46:43 +0300

    Added autogenerated text to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170309115943) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "drivers", force: :cascade do |t|
     t.string "name"
     t.string "license_number"
