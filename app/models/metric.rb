d2a24ddd98daa40d478b64bbf03f5b13c70d7a24
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-10 00:06:25 +0300

    Removed FIXME and fixed validations in Metrics model

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -2,6 +2,5 @@ class Metric < ApplicationRecord
   belongs_to :driver
   self.per_page = 50
 
-  # FIXME: Remove next line if business would like to save garbage metrics
-  validates :metric_name, :value, presence: true
+  validates :driver_id, presence: true
 end
