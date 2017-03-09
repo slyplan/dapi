957593948b27595df4eac26cc06e193e91060bc5
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-09 19:39:44 +0300

    Added fetch_params method to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :fetch_params, only: [:create, :update]
+
+  private
+
+  def fetch_params
+    @json_params = ActiveModelSerializers::Deserialization.jsonapi_parse(JSON.parse(request.body.string))
+  end
 end
