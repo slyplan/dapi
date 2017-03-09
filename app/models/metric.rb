69d581e89704f703f9cbfacc2beeafd6950ea31a
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-09 23:34:20 +0300

    Maked metrics paginable

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,5 +1,6 @@
 class Metric < ApplicationRecord
   belongs_to :driver, optional: true
+  self.per_page = 50
 
   # FIXME: Remove next line if business would like to save garbage metrics
   validates :metric_name, :value, presence: true
