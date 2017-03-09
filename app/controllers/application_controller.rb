8a56ea83c043d36a6717326016b76f64f692aab0
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-09 19:54:40 +0300

    Added header check by specificatiob JSON:API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::API
   before_action :fetch_params, only: [:create, :update]
+  before_action :check_headers, only: [:create, :update]
 
   private
 
   def fetch_params
     @json_params = ActiveModelSerializers::Deserialization.jsonapi_parse(JSON.parse(request.body.string))
+  end
+
+  def check_headers
+    if request.content_type != "application/vnd.api+json"
+      head 406 and return
+    end
   end
 end
