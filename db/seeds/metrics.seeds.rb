f7459dd6daf3a92ff46ec5fec96a2ddfe3438ce2
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-10 00:07:05 +0300

    Added discarded counter to metrics seed

--- db/seeds/metrics.seeds.rb
+++ db/seeds/metrics.seeds.rb
@@ -3,17 +3,16 @@ puts 'Seeding metrics...'
 seed_json_file = 'db/seeds/metrics.json'
 raise "File #{seed_json_file} does not exist" unless File.exist?(seed_json_file)
 raise 'Could not seed, table metrics is not empty' if Metric.first
-
+discarded_metrics = 0
 json_file = File.open(seed_json_file, 'r') do |f|
   f.each_line do |line|
     next if line.chop.empty?
     json_obj = JSON.parse(line)
     object = Metric.new(json_obj)
     unless object.save
-      puts object.errors.messages
-      return
+      discarded_metrics += 1
     end
   end
 end
 
-puts 'Metrics seeded successfully'
+puts "Metrics seeded successfully. Discarded #{discarded_metrics} metrics"
