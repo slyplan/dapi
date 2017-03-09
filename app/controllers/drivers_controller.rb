7fc729e28346d698d2b93a97c832c6e1e0422c2c
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-09 19:40:27 +0300

    Implemented create method for drivers api

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -3,4 +3,13 @@ class DriversController < ApplicationController
     driver = Driver.find(params[:id])
     render json: driver
   end
+
+  def create
+    driver = Driver.new(@json_params)
+    if driver.save
+      render json: driver, status: 201
+    else
+      head 400
+    end
+  end
 end
