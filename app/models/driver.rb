9b784a413ac74dd7f34eb0aa02ff84becdcadc5d
Kirill Kalmykov <kkalmykov@npoprogress.com>
2017-03-10 11:10:24 +0300

    Added dependent: :destroy to relation metrics in Driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,5 @@
 class Driver < ApplicationRecord
-  has_many :metrics
+  has_many :metrics, dependent: :destroy
 
   # FIXME: Remove next line if business would like to save unindentifyed drivers
   validates :name, :license_number, presence: true
